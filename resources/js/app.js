a34626aa2194a41e163e742ecb26d2b3c4244c88
chado <a.sale@outlook.fr>
2023-09-09 10:03:34 +0200

    Project Initialisation

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+import { createApp } from 'vue'
+
+const app = createApp()
+
+Object.entries(import.meta.globEager('./**/*.vue')).forEach(([path, definition]) => {
+    app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default)
+})
+app.mount('#app')
+
+
